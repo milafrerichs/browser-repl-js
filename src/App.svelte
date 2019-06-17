259510a2632b5c7c0543f9586e56a0416049d5e3
milafrerichs <mila.frerichs@gmail.com>
2019-06-17 10:55:53 +0200

    Changed my mind; we dont' need a explanation component
    it should be within the main component, using the chapter and making
    calls to the other compontents
    thanks to svelte-repl :) and the tutorial there

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,4 @@
 <script>
-	import Explanation from './Explanation.svelte';
 	import Viewer from './Viewer.svelte';
 	import Editor from './Editor.svelte';
 
@@ -9,23 +8,37 @@
 	let chapter = {
 		content,
 		title: 'Chapter 1',
-		code: 'document.body.innerHTML = "Test"'
+		code: 'document.body.innerHTML = "Test"',
+		solution: 'document.body.innerHTML = "Solution"'
 	};
 	let code = '';
 	let ready = false;
+	let completed = false;
 	function changeCode(event) {
 		code = event.detail.value;
 	}
 	$: if(ready) {
 		code = chapter.code
 	}
+	function reset() {
+		completed = false;
+		code = chapter.code;
+	}
+	function complete() {
+		completed = true;
+		code = chapter.solution;
+	}
 </script>
 
 <style>
 </style>
 
-<Explanation chapter={chapter} />
-
+<div>
+	{@html chapter.content}
+	<button class="show" on:click="{() => completed ? reset() : complete()}">
+		{completed ? 'Reset' : 'Show me'}
+	</button>
+</div>
 <!-- Editor must habe the code as well -->
 <Editor mode="js" code={code} on:change={changeCode}/>
 <Viewer bind:ready code={code} />
--- src/Explanation.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-	export let chapter = '';
-</script>
-<div>
-	{@html chapter.content}
-</div>
