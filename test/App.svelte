3d28123f3167c7a10cc3c03c399a4860e428e4c6
milafrerichs <mila.frerichs@gmail.com>
2019-06-17 19:13:14 +0200

    Make a svelte component out of it
    add a test directory to test the component out locally
    remove all code that is left over

---
+++ test/App.svelte
@@ -0,0 +1,17 @@
+
+<script>
+	import Tutorials from '../src/Tutorials.svelte';
+	let chapters = [
+		{
+			content: 'Test',
+			code: 'test',
+			solution: 'test'
+		},
+		{
+			content: 'Test 2'
+		}
+	]
+</script>
+
+<Tutorials chapters={chapters} />
+
